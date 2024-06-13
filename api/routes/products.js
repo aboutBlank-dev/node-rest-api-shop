078e82bd0737008217ee9cb359618690b6e15447
aboutBlank-dev <josecolaco1999@gmail.com>
2024-06-13 16:51:21 +0100

    initial commit

---
+++ api/routes/products.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to /products",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(201).json({
+    message: "Handling POST requests to /products",
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: `Handling GET requests to /products/${id}`,
+    id: id,
+  });
+});
+
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: "Handling PATCH request",
+    id: id,
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: "Deleted product!",
+    id: id,
+  });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+//Routes
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+//.use() adds middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Handling CORS errors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+//Handling all routes that do not fall under the ones above
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+//Handle ALL errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
